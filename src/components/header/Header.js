0d772b4214614b0f9fc1239a69efced30c9768bb
Adriana Babakanian <ababakanian@berkeley.edu>
2018-09-19 18:13:34 -0700

    temporarily remove header responsiveness for server side

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -10,39 +10,39 @@ class Header extends React.Component {
     super(props);
     this.toggleNav = this.toggleNav.bind(this);
     this.toggleNavLogo = this.toggleNavLogo.bind(this);
-    this.updateDimensions = this.updateDimensions.bind(this);
+    // this.updateDimensions = this.updateDimensions.bind(this);
     this.state = {
-      showMobileNav: false,
-			width: window.innerWidth,
-			height: window.innerHeight,
+      showMobileNav: false
+			// width: window.innerWidth,
+			// height: window.innerHeight,
     }
   }
 
-  updateDimensions() {
-    this.setState({
-    	width: window.innerWidth,
-    	height: window.innerHeight,
-    });
-  }
-
-  componentWillMount() { this.updateDimensions(); }
-
-  componentDidMount() { window.addEventListener("resize", this.updateDimensions); }
-
-  componentWillUnmount() { window.removeEventListener("resize", this.updateDimensions); }
+  // updateDimensions() {
+  //   this.setState({
+  //   	width: window.innerWidth,
+  //   	height: window.innerHeight,
+  //   });
+  // }
+  //
+  // componentWillMount() { this.updateDimensions(); }
+  //
+  // componentDidMount() { window.addEventListener("resize", this.updateDimensions); }
+  //
+  // componentWillUnmount() { window.removeEventListener("resize", this.updateDimensions); }
 
 	toggleNavLogo() {
-    if (this.state.showMobileNav) {
-      this.setState({
-  			showMobileNav: !this.state.showMobileNav,
-  		})
-    }
+    // if (this.state.showMobileNav) {
+    //   this.setState({
+  	// 		showMobileNav: !this.state.showMobileNav,
+  	// 	})
+    // }
 	}
 
   toggleNav() {
-    this.setState({
-			showMobileNav: !this.state.showMobileNav,
-		})
+    // this.setState({
+		// 	showMobileNav: !this.state.showMobileNav,
+		// })
 	}
 
   render() {
@@ -61,20 +61,21 @@ class Header extends React.Component {
       )),
       mobile_header = (<div/>);
 
-    if (this.state.width < 700) {
-      header_links = links && reveresed.map(link => (
-        <Link to={`/${link[0].toLowerCase()}`}
-              key={link[0]}
-              className="mobile-header-item"
-              activeClassName={`${link[1]}`}
-              onClick={this.toggleNav}>
-          <div className={`mobile-header-item-content ${link[1]}`}>
-            {link[0]}
-          </div>
-        </Link>
-      ));
-      mobile_header = this.state.showMobileNav ? <div className="mobile_header"> {header_links} </div> : null;
-    }
+    // if (this.state.width < 700) {
+    //   header_links = links && reveresed.map(link => (
+    //     <Link to={`/${link[0].toLowerCase()}`}
+    //           key={link[0]}
+    //           className="mobile-header-item"
+    //           activeClassName={`${link[1]}`}
+    //           onClick={this.toggleNav}>
+    //       <div className={`mobile-header-item-content ${link[1]}`}>
+    //         {link[0]}
+    //       </div>
+    //     </Link>
+    //   ));
+    //   mobile_header = this.state.showMobileNav ? <div className="mobile_header"> {header_links} </div> : null;
+    // }
+
     return (
       <div>
         {mobile_header}
@@ -83,7 +84,8 @@ class Header extends React.Component {
             onClick={this.toggleNavLogo}>
             <img src={logo}/>
           </Link>
-          {this.state.width > 700 ? header_links : null}
+          {header_links}
+          {/* {this.state.width > 700 ? header_links : null} */}
           <div className='hamburger'
             onClick={this.toggleNav}>
             {this.state.showMobileNav ? <div>&times;</div> : <div>&#9776;</div>}
