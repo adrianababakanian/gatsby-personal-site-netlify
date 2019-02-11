c5fce2bf3f41c6d3e68c2332568363367ab6225d
Adriana Babakanian <ababakanian@berkeley.edu>
2019-02-10 19:10:24 -0800

    fixed header responsiveness

--- src/components/fact/Fact.js
+++ src/components/fact/Fact.js
@@ -3,11 +3,15 @@ import Link from 'gatsby-link';
 import Tag from './../tag/Tag';
 
 import './Fact.css'
+import zafon from './zafon.png'
 
 const Fact = ({ number, fact, reverse }) => (
   <div className={`fact ${ reverse ? 'reverse' : '' }`}>
     <Tag name={ number } />
     <h5> { fact } </h5>
+    {/* <div className='fact-image'>
+      <img src={zafon} />
+    </div> */}
   </div>
 )
 
